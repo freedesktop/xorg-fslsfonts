e7f14cc37ca82396ca7116efcc9f48a687644ea0
Egbert Eich <eich@suse.de>
2004-02-26 13:36:15 +0000

    readding XFree86's cvs IDs

--- fslsfonts.c
+++ fslsfonts.c
@@ -43,7 +43,7 @@ in this Software without prior written authorization from The Open Group.
  * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
  * THIS SOFTWARE.
  */
-/* $XFree86$ */
+/* $XFree86: xc/programs/fslsfonts/fslsfonts.c,v 3.8 2001/12/14 20:00:45 dawes Exp $ */
 
 #include <X11/fonts/FSlib.h>
 #include <stdio.h>
